fe076eafb1d39447a35eb97fa6bf2b57c3add52f
ParthMasterCoder <69185562+ParthMasterCoder@users.noreply.github.com>
2020-10-05 23:02:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+  
+  createSprite(330,235,30,40);
+  createSprite(360,235,30,40);
+  createSprite(390,235,30,40);
+  createSprite(420,235,30,40);
+  createSprite(450,235,30,40);
+  createSprite(360,195,30,40);
+  createSprite(390,195,30,40);
+  createSprite(420,195,30,40);
+  createSprite(390,155,30,40);
+
+  
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
